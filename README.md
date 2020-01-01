559c20e5042dcbc70dd0c24124949c0ced4d263d
Evan Kaiser <733581+holotrek@users.noreply.github.com>
2020-01-01 12:57:20 -0500

    Feature/link list (#3)
    * Fixes scraper issues that cropped up from bad eval. Integrates the IoC used in game-poller. Adds HTML and Link selection.
    
    * Improves README and changes name of one property

--- README.md
+++ README.md
@@ -20,10 +20,39 @@ yarn start
 
 ## Usage
 
-```
+```js
 const axios = require('axios').default;
-const url = 'https://www.google.com';
-const selector = '#hptl > a:nth-child(1)';
-const result = await axios.get(`http://localhost:3002/api/scrape?url=${url}&selector=${selector}`;
-console.log(result); //'About'
+
+/* Text */
+const url1 = encodeURIComponent('https://boardgamegeek.com/boardgame/264220/tainted-grail-fall-avalon');
+const selector1 = encodeURIComponent('div.game-header-title-info h1 a');
+const result1 = await axios.get(`http://localhost:3002/api/scrape/text?url=${url1}&selector=${selector1}`);
+/* Result: Array of all elements matching selector
+    [
+        { content: 'Inner text (without HTML) of element' }
+    ]
+*/
+
+/* HTML */
+const url2 = encodeURIComponent('https://boardgamegeek.com/boardgame/264220/tainted-grail-fall-avalon');
+const selector2 = encodeURIComponent('article.game-description-body p');
+const result2 = await axios.get(`http://localhost:3002/api/scrape/html?url=${url2}&selector=${selector2}`);
+/* Result: Array of all elements matching selector
+    [
+        { content: 'Inner HTML of element' }
+    ]
+*/
+
+/* Link */
+const url3 = encodeURIComponent('https://boardgamegeek.com/browse/boardgame');
+const selector3 = encodeURIComponent('#collectionitems td:nth-child(3) a');
+const result3 = await axios.get(`http://localhost:3002/api/scrape/link?url=${url3}&selector=${selector3}`);
+/* Result: Array of all elements matching selector
+    [
+        {
+            content: 'Inner text (without HTML) of element',
+            link: 'The URL that the link is pointing to for this anchor element'
+        }
+    ]
+*/
 ```
---
+++ src/controllers/puppeteer-controller.js
@@ -0,0 +1,60 @@
+export class PuppeteerController {
+    constructor({ puppeteerService }) {
+        this.service = puppeteerService;
+    }
+
+    async scrapeText(req, res) {
+        try {
+            const results = await this.service.eval(req.query.url, req.query.selector, sel =>
+                Array.from(document.querySelectorAll(sel)).map(x => ({
+                    content: x.innerText.trim()
+                }))
+            );
+            if (results.error) {
+                res.status(400).send(results);
+            } else {
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+    }
+
+    async scrapeHtml(req, res) {
+        try {
+            const results = await this.service.eval(req.query.url, req.query.selector, sel =>
+                Array.from(document.querySelectorAll(sel)).map(x => ({
+                    content: x.innerHTML.trim()
+                }))
+            );
+            if (results.error) {
+                res.status(400).send(results);
+            } else {
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+    }
+
+    async scrapeLink(req, res) {
+        try {
+            const results = await this.service.eval(req.query.url, req.query.selector, sel =>
+                Array.from(document.querySelectorAll(sel)).map(x => ({
+                    content: x.innerText.trim(),
+                    link: x.getAttribute('href')
+                }))
+            );
+            if (results.error) {
+                res.status(400).send(results);
+            } else {
+                res.send(results);
+            }
+        } catch (err) {
+            console.error(err);
+            res.sendStatus(500);
+        }
+    }
+}
